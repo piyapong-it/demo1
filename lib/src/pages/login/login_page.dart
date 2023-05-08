bafa115399ab9fb7abc6045b46308bbe798feea0
FERGYDOMAIN\piyapongs <piyapongs@tapb.co.th>
2023-05-08 16:01:27 +0700

    :: stateless login_form

--- lib/src/pages/login/login_page.dart
+++ lib/src/pages/login/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/login_form.dart';
+
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
@@ -10,15 +12,13 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
+  int count = 0;
+
+
 
   @override
-  void initState() {
-    super.initState();
-    _usernameController.text = 'piyapongs';
-    _passwordController.text = '1234';
-  }
-  @override
   Widget build(BuildContext context) {
+    print("CALL");
     return Scaffold(
       body: Column(
         children: [
@@ -26,61 +26,10 @@ class _LoginPageState extends State<LoginPage> {
             height: 80,
           ),
           Image.asset("assets/images/logo.png"),
-          _buildForm()
+          LoginForm()
         ],
       ),
     );
   }
 
-  _buildForm() {
-    return Padding(
-      padding: const EdgeInsets.only(top: 20.0, left: 30, right: 30),
-      child: Card(
-        elevation: 7,
-        child: Padding(
-          padding: const EdgeInsets.all(18.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              // Username
-              TextField(
-                obscureText: true,
-                controller: _usernameController,
-                decoration: InputDecoration(
-                    border: InputBorder.none,
-                    labelText: 'UserName',
-                    icon: Icon(Icons.email)),
-                // keyboardType: TextInputType.emailAddress,
-              ),
-              // SizedBox(10),
-              // password
-              TextField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                    border: InputBorder.none,
-                    labelText: 'Password',
-                    icon: Icon(Icons.lock)),
-                keyboardType: TextInputType.number,
-              ),
-              // login button
-              ElevatedButton(
-                  onPressed: () {
-                    print(
-                        "login :${_usernameController.text}, ${_passwordController.text}");
-                  },
-                  child: Text("Login")),
-              // Register
-              OutlinedButton(
-                  onPressed: () {
-                    final username = _usernameController.text;
-                    final password = _passwordController.text;
-                    print("Register: $username, $password");
-                  }, child: Text("Register"))
-            ],
-          ),
-        ),
-      ),
-    );
-  }
 }
---
+++ lib/src/pages/login/widgets/login_form.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class LoginForm extends StatelessWidget {
+  LoginForm({super.key});
+
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20.0, left: 30, right: 30),
+      child: Card(
+        elevation: 7,
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // Username
+              TextField(
+                obscureText: true,
+                controller: _usernameController,
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    labelText: 'UserName',
+                    icon: Icon(Icons.email)),
+                // keyboardType: TextInputType.emailAddress,
+              ),
+              // SizedBox(10),
+              // password
+              TextField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    labelText: 'Password',
+                    icon: Icon(Icons.lock)),
+                keyboardType: TextInputType.number,
+              ),
+              // login button
+              ElevatedButton(
+                  onPressed: () {
+                    final username = _usernameController.text;
+                    final password = _passwordController.text;
+                    print("Login: $username, $password");
+                  },
+                  child: Text("Login")),
+              // Register
+              OutlinedButton(
+                  onPressed: () {
+                    final username = _usernameController.text;
+                    final password = _passwordController.text;
+                    print("Register: $username, $password");
+                  },
+                  child: Text("Register"))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
