d7cf652eea6448f62f5798bbdddad8f6d4c3b583
FERGYDOMAIN\piyapongs <piyapongs@tapb.co.th>
2023-05-12 15:17:27 +0700

    การแสดง logger โชว์ error

--- lib/src/bloc/counter/counter_bloc.dart
+++ lib/src/bloc/counter/counter_bloc.dart
@@ -1,6 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
+import '../../app.dart';
+
 part 'counter_event.dart';
 part 'counter_state.dart';
 
@@ -8,6 +10,7 @@ class CounterBloc extends Bloc<CounterEvent, CounterState> {
   CounterBloc() : super(CounterState()) {
     // Add
     on<CounterEventAdd>((event, emit) {
+      logger.d("Count ++: ${state.count1}");
       emit(state.copyWith(count1: state.count1 + 1, count2: state.count2 + 1));
     });
 
